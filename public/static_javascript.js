ae33b8bee5378a9f503c88a514f1c7003cfedaf2
steven-ha <has@oregonstate.edu>
2015-12-06 22:35:05 -0600

    update

--- public/static_javascript.js
+++ public/static_javascript.js
@@ -1,1 +1,19 @@
+function addRow(){
+    var req = new XMLHttpRequest();
+	
+	var addName = steven;//document.getElementById("name").value;
+    var addReps = 3;//document.getElementById("reps").value;
+	var addWeight = document.getElementById("weight").value;
+    var addDate = document.getElementById("date").value;
+    var addLbs = document.getElementById("lbs").value;
+// + "&weight=" + addWeight + "&date=" + addDate + "&lbs=" + addLbs
+	
+	req.open("GET", "http://52.10.69.111:3000/insert?name=" + addName + "&reps=" + addReps, true);
+	req.setRequestHeader("Content-type", "application/json");
+	req.addEventListener("load", function(){
+		console.log("Row Insert into Table");
+	});
+	req.send(null);	
+}
 
+document.getElementById("submitData").addEventListener("click", addRow);
